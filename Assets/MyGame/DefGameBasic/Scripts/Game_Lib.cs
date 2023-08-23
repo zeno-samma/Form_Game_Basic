7182aa0bf4fadb86ba2c4360e643503a4cf17811
Xuyen <tranvanxuyen111@gmail.com>
2023-08-24 00:49:05 +0700

    Update
    Update

---
+++ Assets/MyGame/DefGameBasic/Scripts/Game_Lib.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Lib : MonoBehaviour
+{
+    public Transform myTransform;
+    public void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        myTransform.position = new Vector3(5,0,0);
+        //Debug.Log(gameObject.tag);
+    }
+    private void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
